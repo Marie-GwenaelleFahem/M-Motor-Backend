87bc80fdbfc914b348a0c7ffc73688c83e083fb9
Julie Luciani <julie.lcny@gmail.com>
2025-02-20 14:37:08 +0100

    add cors authorisations

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from controllers import router
 from database import database
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup():
